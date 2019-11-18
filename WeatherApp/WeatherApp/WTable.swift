5e49998e1d646bf8f02ccbb9e7d0479d79e5e105
Kishan Patel <kishanpatel@student-wireless-t-1983.fdu.edu>
2019-11-18 12:24:55 -0500

    First Upload

---
+++ WeatherApp/WeatherApp/WTable.swift
@@ -0,0 +1,13 @@
+//
+//  WTable.swift
+//  WeatherApp
+//
+//  Created by Kishan Patel on 11/8/19.
+//  Copyright © 2019 Kishan Patel. All rights reserved.
+//
+
+import UIKit
+
+class WTable: WeatherUIView {
+
+}
